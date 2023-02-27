b1a526b5ff98451773f51aefa404a0db91bfd082
DM <dommcneill@outlook.com>
2023-02-27 12:32:43 +0000

    IT WORKS

---
+++ Gold Assault/Assets/Scripts/Items/Guns/Gun.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Gun : Item
+{
+    public abstract override void UseMouse0();
+    public abstract override void UseRKey();
+
+    //public GameObject bulletImpactPrefab;
+}
