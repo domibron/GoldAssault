3843edeae5cee121de6fc4af182354bd17d87e6f
DM <dommcneill@outlook.com>
2023-02-28 13:46:39 +0000

    the game starts in windowed mode

--- Gold Assault/Assets/Scripts/UI/WarnSetup.cs
+++ Gold Assault/Assets/Scripts/UI/WarnSetup.cs
@@ -8,7 +8,7 @@ public class WarnSetup : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        GetComponent<TMP_Text>().text = $"<b>DO NOT DISTRUBUTE</b><br>Game Version: {Application.version}"
+        GetComponent<TMP_Text>().text = $"<b>DO NOT DISTRIBUTE</b><br>Game Version: {Application.version}"
         + "<br>This is a prototype build and is not ment for the public.<br>Everything is subject to change!";
     }
 }
