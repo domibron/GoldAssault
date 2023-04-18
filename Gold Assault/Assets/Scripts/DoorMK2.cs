81842414e7986528d60a8e8b47156fd92abd0816
DM <dommcneill@outlook.com>
2023-04-18 15:03:08 +0100

    doors work to a degree

--- Gold Assault/Assets/Scripts/DoorMK2.cs
+++ Gold Assault/Assets/Scripts/DoorMK2.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DoorMK2 : MonoBehaviour
 {
     public GameObject door;
+    public GameObject? door2;
 
-    public float timeDelay = 0.1f;
+    public float timeDelay = 0.2f;
+    public float timeDelayOther = 0.2f;
+
+    public float power = 3f;
 
     private bool isOpen = false;
+    private bool isOtherDoorOpen = false;
 
     private float currentTime = 0;
+    private float currentTimeOther = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,15 +25,30 @@ public class DoorMK2 : MonoBehaviour
 
     }
 
+    //* AI incoperation, the AI cannot see the door, they will attempt to go through them.
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "AI")
+        {
+            if (!isOpen && !isOtherDoorOpen)
+            {
+                OpenAndCloseBothDoors(other.transform);
+            }
+            else if (isOpen && !isOtherDoorOpen)
+            {
+                OpenAndCloseOtherDoor(other.transform);
+            }
+            else if (!isOpen && isOtherDoorOpen)
+            {
+                OpenAndClose(other.transform);
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        // if (!isOpen)
-        // {
-        // 	print(Quaternion.Dot(door.transform.localRotation, Quaternion.Euler(0, 0, 0)));
-
-
-        // }
 
         if (currentTime <= timeDelay)
             currentTime += Time.deltaTime;
@@ -37,35 +59,128 @@ public class DoorMK2 : MonoBehaviour
             rb.velocity = Vector3.zero;
             door.transform.rotation = Quaternion.identity;
         }
+
+        if (door2 != null)
+        {
+            if (currentTimeOther <= timeDelayOther)
+                currentTimeOther += Time.deltaTime;
+
+            if (Quaternion.Dot(door2.transform.localRotation, Quaternion.Euler(0, 0, 0)) >= 0.995f && currentTimeOther >= timeDelayOther)
+            {
+                Rigidbody rb = door2.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.zero;
+                door2.transform.rotation = Quaternion.identity;
+            }
+        }
     }
 
     public void OpenAndClose(Transform player)
     {
+        DoorOpenClose(player);
+    }
+
+    public void OpenAndCloseOtherDoor(Transform player)
+    {
+        if (door2 == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        DoorOpenCloseOther(player);
+    }
+
+    private void DoorOpenClose(Transform player)
+    {
         if (Quaternion.Dot(door.transform.localRotation, Quaternion.Euler(0, 0, 0)) >= 0.995f)
         {
-            isOpen = true;
+            isOpen = false;
         }
         else
         {
-            isOpen = false;
+            isOpen = true;
         }
 
         currentTime = 0;
 
         Vector3 dirWithPlayer = door.transform.position - player.position;
         Vector3 dir = door.transform.position - transform.position;
+
         dirWithPlayer.Normalize();
         dir.Normalize();
 
+        Rigidbody rb = door.GetComponent<Rigidbody>();
+
         if (isOpen)
         {
-            Rigidbody rb = door.GetComponent<Rigidbody>();
-            rb.AddForce(-dir * 5f, ForceMode.Impulse);
+            if (Quaternion.Dot(door.transform.localRotation, Quaternion.Euler(0, 90, 0)) > 0.7f)
+            {
+                rb.AddForce(door.transform.forward * (power * 0.8f), ForceMode.Impulse);
+            }
+            else if (Quaternion.Dot(door.transform.localRotation, Quaternion.Euler(0, 90, 0)) < 0.7f)
+            {
+                rb.AddForce(-door.transform.forward * (power * 0.8f), ForceMode.Impulse);
+            }
         }
         else
         {
-            Rigidbody rb = door.GetComponent<Rigidbody>();
-            rb.AddForce(dirWithPlayer * 5f, ForceMode.Impulse);
+            rb.AddForce(dirWithPlayer * power, ForceMode.Impulse);
         }
+    }
+
+    private void DoorOpenCloseOther(Transform player)
+    {
+        if (Quaternion.Dot(door2.transform.localRotation, Quaternion.Euler(0, 0, 0)) >= 0.995f)
+        {
+            isOtherDoorOpen = false;
+        }
+        else
+        {
+            isOtherDoorOpen = true;
+        }
+
+        currentTimeOther = 0;
+
+        Vector3 dirWithPlayer = door2.transform.position - player.position;
+        Vector3 dir = door2.transform.position - transform.position;
+
+        dirWithPlayer.Normalize();
+        dir.Normalize();
+
+        Rigidbody rb = door2.GetComponent<Rigidbody>();
+
+        if (isOtherDoorOpen)
+        {
+            if (Quaternion.Dot(door2.transform.localRotation, Quaternion.Euler(0, -90, 0)) > 0.7f)
+            {
+                rb.AddForce(door2.transform.forward * (power * 0.8f), ForceMode.Impulse);
+            }
+            else if (Quaternion.Dot(door2.transform.localRotation, Quaternion.Euler(0, -90, 0)) < 0.7f)
+            {
+                rb.AddForce(-door2.transform.forward * (power * 0.8f), ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            rb.AddForce(dirWithPlayer * power, ForceMode.Impulse);
+        }
+    }
+
+    public void OpenAndCloseBothDoors(Transform player)
+    {
+        if (door2 == null)
+        {
+            throw new NullReferenceException();
+        }
+
+        if (!isOpen && isOtherDoorOpen)
+            DoorOpenClose(player);
+        else if (isOpen && !isOtherDoorOpen)
+            DoorOpenCloseOther(player);
+        else
+        {
+            DoorOpenClose(player);
+            DoorOpenCloseOther(player);
+        }
+
     }
 }
